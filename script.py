5447b1b92363a6b0f39ad95a91e4b3a890d15db2
Shivansh Gupta <gupta.shivansh98@gmail.com>
2018-10-12 16:05:44 +0530

    Final

--- script.py
+++ script.py
@@ -4,27 +4,29 @@ import urllib.request
 import re
 from urllib.parse import urlparse
 import tldextract
+#various imports during the assignment
 
-def get_count_url(url):
+def get_count_url(url): # get the umber of links having the same domain and suffix
 	r = requests.get(url)
 	soup = BeautifulSoup(r.text, "html.parser")
 	count = 0
-	urls={}
+	urls={} #dictionary for the domains
 	# input_domain=url.split('//')[1].split('/')[0]
-	input_domain=tldextract.extract(url).domain+"."+tldextract.extract(url).suffix
+	#library to extract the exact domain( ex.- blog.bbc.com and bbc.com have the same domains )
+	input_domain=tldextract.extract(url).domain+"."+tldextract.extract(url).suffix 
 	for link in soup.find_all('a'):
-		# print(link.get('href'))
 		word =link.get('href')
 		# print(word)
-		if word is not None:
+		if word:
 			# Same website or domain calls
-			if "#" in word or word[0]=="/":
+			if "#" in word or word[0]=="/": #div call or same domain call
 				if not input_domain in urls:
 					# print(input_domain)
-					urls[input_domain]=1
+					urls[input_domain]=1 #if first encounter with the domain
 				else:
-					urls[input_domain]+=1
+					urls[input_domain]+=1 #multiple encounters
 			elif "javascript" in word:
+				# javascript function calls (for domains that use modern JS frameworks to display information)
 				if not "JavascriptRenderingFunctionCall" in urls:
 					urls["JavascriptRenderingFunctionCall"]=1
 				else:
@@ -34,23 +36,19 @@ def get_count_url(url):
 				main_domain=tldextract.extract(word).domain+"." +tldextract.extract(word).suffix
 				# print(main_domain)
 				if main_domain.split('.')[0]=='www':
-					main_domain = main_domain.replace("www.","")
-					# print(domain)
-				if not main_domain in urls:
+					main_domain = main_domain.replace("www.","") # removing the www
+				if not main_domain in urls: # maintaining the dictionary
 					urls[main_domain]=1
 				else:
 					urls[main_domain]+=1
-			# main_domain=tldextract.extract(word).domain
-			# print(word)
-			# print(domain)
 			count += 1
 	
-	for key, value in urls.items():
+	for key, value in urls.items(): # printing the dictionary in a paragraph format for better readability
 	 	print(key,value)
 	return count	
 
 
-def get_url_info(url):
+def get_url_info(url): # function to get size of web page in bytes
     d = urllib.request.urlopen(url)
     return len(d.read())
 
